9616714a003bc31485a22beec185f09f09d85762
m3llad0 <a01655451@tec.mx>
2023-09-09 17:47:00 -0600

    function to give currency format to a number

---
+++ frontend/src/app/utils/formatCurrency.ts
@@ -0,0 +1,19 @@
+/**
+ * Formats a number as a currency string in Mexican Peso (MXN) format.
+ * 
+ * @param value - The number to be formatted as currency.
+ * @returns A formatted currency string in Mexican Peso (MXN) format.
+ * @throws {Error} If the value parameter is not a number.
+ * 
+ * @example
+ * const amount = 1000;
+ * const formattedAmount = formatCurrency(amount);
+ * console.log(formattedAmount); // Output: $1,000.00
+ */
+export const formatCurrency = (value: number) => {
+    if (typeof value !== 'number') {
+        throw new Error('Invalid value. Expected a number.');
+    }
+
+    return new Intl.NumberFormat('es-MX', { style: 'currency', currency: 'MXN' }).format(value);
+}
