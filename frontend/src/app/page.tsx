017b9772d0a50c7491e1adc5f35c37364bfb0a30
m3llad0 <a01655451@tec.mx>
2023-09-11 16:42:34 -0600

    changed directories location

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,7 +1,18 @@
+import React from "react"
+import { HouseCard } from "@/components/house-card/houseCard"
 export default function Home() {
+
+  const houseData = {
+    id: 1,
+    img: "https://www.bhg.com/thmb/H9VV9JNnKl-H1faFXnPlQfNprYw=/1799x0/filters:no_upscale():strip_icc()/white-modern-house-curved-patio-archway-c0a4a3b3-aa51b24d14d0464ea15d36e05aa85ac9.jpg", 
+    title: "House 1",
+    buy_price: 100000,
+    future_value: 200000,
+  } 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24 font-bold text-8xl">
-      HouseStock
-    </main>
-  )
+  <>
+    <div className="flex flex-wrap">
+      <HouseCard {...houseData} />      
+    </div>
+  </>  )
 }
