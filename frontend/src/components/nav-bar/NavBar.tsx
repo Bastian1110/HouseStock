0e8230fd05b1649a5959748a4a6efc99689b8a3b
AlanSaid1 <99698053+AlanSaid1@users.noreply.github.com>
2023-09-13 18:55:11 -0600

    menu dropdown listo

--- frontend/src/components/nav-bar/NavBar.tsx
+++ frontend/src/components/nav-bar/NavBar.tsx
@@ -1,29 +1,38 @@
-import React from "react";
+"use client"
+import React, { useState, useEffect } from "react";
+
 import ProfileImage from '@/components/images/foto_perfil.jpg'
 
 export default function NavBar() {
-  return (
+  const [isUserMenuOpen, setUserMenuOpen] = useState(false);
 
+  const toggleUserMenu = () => {
+    setUserMenuOpen(!isUserMenuOpen);
+  };
+
+  useEffect(() => {
+    console.log("isUserMenuOpen:", isUserMenuOpen);
+  }, [isUserMenuOpen]);
+
+  return (
     <nav className="bg-white border-gray-200 dark:bg-gray-900">
-      <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+    <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
       <a href="https://flowbite.com/" className="flex items-center">
-          <img src="https://flowbite.com/docs/images/logo.svg" className="h-8 mr-3" alt="HouseStock logo" />
-          <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">HouseStock</span>
+        <img src="https://flowbite.com/docs/images/logo.svg" className="h-8 mr-3" alt="HouseStock logo" />
+        <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">HouseStock</span>
       </a>
       
       <div className="flex items-center md:order-2">
-          <button type="button" className="flex mr-3 text-sm bg-gray-800 rounded-full md:mr-0 focus:ring-4 focus:ring-gray-300 dark:focus:ring-gray-600" id="user-menu-button" aria-expanded="false" data-dropdown-toggle="user-dropdown" data-dropdown-placement="bottom">
+          <button onClick={toggleUserMenu} type="button" style= {{position: 'relative'}} aria-expanded={isUserMenuOpen} className="flex mr-3 text-sm bg-gray-800 rounded-full md:mr-0 focus:ring-4 focus:ring-gray-300 dark:focus:ring-gray-600" id="user-dropdown" >
             <span className="sr-only">Open user menu</span>
             <img className="w-8 h-8 rounded-full" src={ProfileImage.src} alt="foto adrian"/>
           </button>
 
-
-          <div className="z-50 hidden my-4 text-base list-none bg-white divide-y divide-gray-100 rounded-lg shadow dark:bg-gray-700 dark:divide-gray-600" id="user-dropdown">
-            <div className="px-4 py-3">
+          <div className={`z-50 absolute right-0 mt-0 top-0 ${isUserMenuOpen ? 'block' : 'hidden'} text-base list-none bg-white divide-y divide-gray-100 rounded-lg shadow dark:bg-gray-700 dark:divide-gray-600`} id="user-dropdown"><div className="px-4 py-3">
               <span className="block text-sm text-gray-900 dark:text-white">Adrian Lopez</span>
               <span className="block text-sm  text-gray-500 truncate dark:text-gray-400">AdrianBravo@gmail.com</span>
             </div>
-            <ul className="py-2" aria-labelledby="user-menu-button">
+            <ul className="py-2" aria-labelledby="user-dropdown">
               <li>
                 <a href="#" className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Dashboard</a>
               </li>
@@ -39,12 +48,14 @@ export default function NavBar() {
             </ul>
           </div>
           <button data-collapse-toggle="navbar-user" type="button" className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-user" aria-expanded="false">
+
             <span className="sr-only">Open main menu</span>
             <svg className="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
                 <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 1h15M1 7h15M1 13h15"/>
             </svg>
         </button>
       </div>
+
       <div className="items-center justify-between hidden w-full md:flex md:w-auto md:order-1" id="navbar-user">
         <ul className="flex flex-col font-medium p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
           <li>
@@ -69,5 +80,3 @@ export default function NavBar() {
 
   )
 }
-
-
