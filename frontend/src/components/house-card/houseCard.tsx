c1d469034304c9bd42aca6587da1da5a490d8f15
m3llad0 <a01655451@tec.mx>
2023-09-13 23:22:22 -0600

    using id as searching param

--- frontend/src/components/house-card/houseCard.tsx
+++ frontend/src/components/house-card/houseCard.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { formatCurrency } from '@/utils/formatCurrency';
-
+import Link from 'next/link';
 type HouseData = {
     id: number,
     img: string,
@@ -17,21 +17,26 @@ type HouseData = {
 export function HouseCard(house: HouseData){
 
     return(
-        <a href={`/house/${house.id}`} className="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-            <img className="rounded-t-lg" src={house.img} alt={house.title}/>
-            <div className="p-5">
-                <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{house.title}</h5>
-                <span className='flex flex-row'>
-                    <div className='flex flex-col w-full'>
-                        <p className='text-black-700 dark:text-white-400 text-sm'>Compra</p>
-                        <p className='text-gray-700 dark:text-gray-400 text-base'>{formatCurrency(house.buy_price)}</p>
-                    </div>
-                    <div className='flex flex-col'>
-                        <p className='text-black-700 dark:text-white-400 text-sm'>Posible venta</p>
-                        <p className='text-gray-700 dark:text-gray-400 text-base'>{formatCurrency(house.future_value)}</p>
-                    </div>
-                </span>
-            </div>
-        </a> 
+        <Link href={{
+            pathname: '/house',
+            query: { id: house.id },
+        }}>
+            <div className="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+                <img className="rounded-t-lg" src={house.img} alt={house.title}/>
+                <div className="p-5">
+                    <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{house.title}</h5>
+                    <span className='flex flex-row'>
+                        <div className='flex flex-col w-full'>
+                            <p className='text-black-700 dark:text-white-400 text-sm'>Compra</p>
+                            <p className='text-gray-700 dark:text-gray-400 text-base'>{formatCurrency(house.buy_price)}</p>
+                        </div>
+                        <div className='flex flex-col'>
+                            <p className='text-black-700 dark:text-white-400 text-sm'>Posible venta</p>
+                            <p className='text-gray-700 dark:text-gray-400 text-base'>{formatCurrency(house.future_value)}</p>
+                        </div>
+                    </span>
+                </div>
+            </div> 
+        </Link>
     )
 }
